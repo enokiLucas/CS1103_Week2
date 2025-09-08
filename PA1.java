4ec6fdc3d608cdff99e928e309b2cb6858d00632
Lucas Enoki <enoki.lucas@gmail.com>
2025-09-08 07:01:13 -0300

    Programming Assign 1

---
+++ PA1.java
@@ -0,0 +1,53 @@
+/**
+ * Scenario: You have been asked to create a text analysis tool that will perform various operations on a given text input. This tool will help users gain insights into the text data by performing character and word analysis. 
+
+
+ Assignment Tasks: 
+
+
+ User Input: Ask the user to input a paragraph or a lengthy text. Your program should read and store this input. 
+
+ Character Count: Calculate and display the total number of characters in the input text. 
+
+ Word Count: Calculate and display the total number of words in the input text. Assume that words are separated by spaces. 
+
+ Most Common Character: Find and display the most common character in the text. In case of a tie, select any of the tied characters. 
+
+ Character Frequency: Ask the user to input a character. Check and display the frequency of occurrences of this character in the text. Be case-insensitive (e.g., 'a' and 'A' should be considered the same character). 
+
+ Word Frequency: Ask the user to input a word. Check and display the frequency of occurrences of this word in the text. Be case-insensitive. 
+
+ Unique Words: Calculate and display the number of unique words in the text (case-insensitive).
+
+ *
+ */
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class PA1 {
+
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        String userInput;
+
+        System.out.println("Please input a paragraph or a lengthy text");
+        userInput = stdin.nextLine();
+        //System.out.println(userInput);
+
+        stdin.close();
+
+        // Character Count
+        int userInputLength = userInput.length();
+        System.out.println("Character Count: "+ userInputLength);
+
+        // Word count
+        // Most Common Character
+        // Character Frequency
+        // Word Frequency
+        // Unique Words
+
+    }
+}
